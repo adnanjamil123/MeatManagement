3fbbb3019fbff1b8a0f32e35774676a694da5b80
adnanjamil123 <49995849+adnanjamil123@users.noreply.github.com>
2020-12-23 19:09:42 +0300

    Update index.php

--- Sales/index.php
+++ Sales/index.php
@@ -156,12 +156,7 @@ if(!isset($_SESSION["active"]))
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <!-- <tr>
-                                    <th scope="row">1</th>
-                                    <td>Mark</td>
-                                    <td>Otto</td>
-                                    <td>@mdo</td>
-                                    </tr> -->
+                                    
                                 </tbody>
                                 </table>
 
