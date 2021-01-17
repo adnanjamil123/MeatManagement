fd2848f0727767c4306806a825d030ab95fe2ae3
gameplay2050 <brainfastmans2050@gmail.com>
2021-01-17 21:37:33 +0300

    login page change

--- Sales/Login.php
+++ Sales/Login.php
@@ -9,6 +9,20 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   </head>
+  
+  <style>
+      
+      input{
+          padding:10px;
+          width:310px;
+      }
+      
+      button {
+           padding:10px;
+          width:400px;
+      }
+      
+  </style>
   <body>
       
     <!-- Optional JavaScript -->
@@ -17,23 +31,26 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   
-  
+  <center>
     <div class="id_frm container-fluid">
     
-    <h2>Log In</h2>
+    <h2 style="background:black; color:white; padding:10px;">Login as a Cashier</h2>
+    <br/>
     <form action="includes/login.inc.php" method ="POST" class="container-fluid">
-        <p>
-            <label for="username">username:</label>
-            <input type="text" id="username" name="username" placeholder= "please enter username"/>            
+        <p> 
+            <label for="username">Username:</label> &nbsp;
+            <input type="text" id="username" name="username" placeholder= "Username" required/>            
         </p>
-        <p>
-            <label for="username">password:</label>
-            <input type="password" id="password" name="pwd"/>            
+        <p> 
+            <label for="username">Password:</label>  &nbsp;
+            <input type="password" id="password" name="pwd" placeholder= "Password" required/>            
         </p>
         <p>
             <button type="submit" name="submit">Log In</button>     
         </p>
     </form>
+    
+   
     <?php
 
 if(isset($_GET["error"]))
@@ -64,6 +81,6 @@ if(isset($_GET["error"]))
 }
 ?>
 </div>
-  
+  </center>
   </body>
 </html>
